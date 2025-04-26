b1ac98ec369c5beb904b49a02aff87f0f2147bfd
Gustavo1701 <gustavojr.lg.10@gmail.com>
2025-04-26 11:50:28 -0300

    Corrige duplicação da rota /localizacoes

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,27 @@ const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/localizacao.routes.js');
 const db = require('./config/db.config.js');
+const path = require('path');  // Importa o módulo path do Node.js
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
 
 // Middlewares
 app.use(cors());
 app.use(express.json()); // substitui body-parser
 
+// Serve arquivos estáticos da pasta 'frontend' (caminho absoluto)
+app.use(express.static(path.join(__dirname, '../frontend')));
+
 // Rotas
 app.use('/localizacoes', routes);
+
+// Rota para página principal: retorna o index.html do frontend
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
 
 // Função para iniciar o servidor
 const startServer = async () => {
